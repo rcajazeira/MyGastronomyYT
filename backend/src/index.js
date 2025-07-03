f83bae224323b93ade3c22fe9a3eaf89179e969d
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2025-07-03 02:11:50 -0300

    Projeto já iniciado com index, json e pasta backend

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import { Mongo } from './database/mongo.js'
+import { config } from 'dotenv'
+
+config();
+
+
+async function main() {
+
+  const hostname = 'localhost'
+  const port = 3000
+  const app = express()
+  
+  // Agora process.env.MONGO_CS e process.env.MONGO_DB_NAME terão os valores do .env
+  const mongoConnection = await Mongo.connect({ mongoConnectionString: process.env.MONGO_CS, mongoDbName: process.env.MONGO_DB_NAME })
+  
+  // O seu console.log(mongoConnection) aqui deveria mostrar "Connected to mongo" se tudo der certo
+  console.log(mongoConnection) 
+  
+  app.use(express.json())
+  app.use(cors())
+
+  app.get('/', (req, res) => {
+    res.send({
+      sucess: true,
+      statusCode: 200,
+      body: 'Welcome to MyGastronomy!'
+    })
+  })
+
+  app.listen(port, () => {
+    console.log(`Server running on: http://${hostname}:${port}`)
+  })
+}
+main()
