425fb5ac81f5b089a098469050638303466fcff7
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2025-07-04 01:01:19 -0300

    autenticação de email sucesso

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -37,3 +37,98 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, passwor
         return callback(null, rest)
     })
 }))
+
+authRouter.post('/signup', async(req, res) => {
+    const checkUser = await Mongo.db
+    .collection(collectionName)
+    .findOne({ email: req.body.email })
+
+    if(checkUser) {
+        return res.status(500).send({
+            success: false,
+            statusCode: 500,
+            body: {
+                text: 'User already exists'
+            }
+        })
+    }
+
+    const salt = crypto.randomBytes(16)
+    
+    crypto.pbkdf2(req.body.password, salt, 310000, 16, 'sha256', async (error, hashedPassword) => {
+        if(error) {
+            res.status(500).send({
+                success: false,
+                statusCode: 500,
+                body: {
+                    text: 'User already exists'
+                }
+            })
+        }
+
+         const result = await Mongo.db
+        .collection(collectionName)
+        .insertOne({
+            fullname: req.body.fullname,
+            email: req.body.email,
+            password: hashedPassword,
+            salt,
+        })
+
+        if(result.insertedId) {
+            const user = await Mongo.db
+            .collection(collectionName)
+            .findOne({ _id: new ObjectId(result.insertedId) }, { projection: { password: 0, salt: 0 } })
+
+            const token = jwt.sign(user, 'secret')
+
+            return res.send({
+                success: true,
+                statusCode: 200,
+                body: {
+                    text: 'User registered',
+                    user,
+                    token
+                }
+            })
+        }
+    })
+})
+
+authRouter.post('/login', (req, res) => {
+    passport.authenticate('local', (error, user) => {
+        if(error) {
+            return res.status(500).send({
+                success: false,
+                statusCode: 500,
+                body: {
+                    text: 'Error during authentication',
+                    error
+                }
+            })
+        }
+
+        if(!user) {
+            return res.status(400).send({
+                success: false,
+                statusCode: 400,
+                body: {
+                    text: 'Credentials are not correct',
+                }
+            })  
+        }
+
+        const token = jwt.sign(user, 'secret')
+        return res.status(200).send({
+            success: true,
+            statusCode: 200,
+            body: {
+                text: 'User logged in correctly',
+                user,
+                token
+            }
+        })  
+    })(req, res)
+})
+
+export default authRouter
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { Mongo } from './database/mongo.js'
 import { config } from 'dotenv'
+import authRouter from './auth/auth.js'
 
 config();
 
@@ -28,6 +29,9 @@ async function main() {
       body: 'Welcome to MyGastronomy!'
     })
   })
+
+   // routes
+    app.use('/auth', authRouter)
 
   app.listen(port, () => {
     console.log(`Server running on: http://${hostname}:${port}`)
