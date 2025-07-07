efe21eefbd0264f310a0e299f8be8abff9e539dc
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2025-07-07 00:01:43 -0300

    Ajuste de plates e orders pastas e suas devidas rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import { Mongo } from './database/mongo.js'
 import { config } from 'dotenv'
 import authRouter from './auth/auth.js'
 import usersRouter from './routes/users.js'
+import platesRouter from './routes/plates.js'
+import ordersRouter from './routes/orders.js'
 
 config();
 
@@ -34,9 +36,14 @@ async function main() {
    // routes
     app.use('/auth', authRouter)
     app.use('/users', usersRouter)
-    /*app.use('/plates', platesRouter)
-    app.use('/orders', ordersRouter)*/
+    app.use('/plates', platesRouter)
+    app.use('/orders', ordersRouter)
 
+
+   
+    /*
+    Na Pista e Colou Certo
+     */
   app.listen(port, () => {
     console.log(`Server running on: http://${hostname}:${port}`)
   })
