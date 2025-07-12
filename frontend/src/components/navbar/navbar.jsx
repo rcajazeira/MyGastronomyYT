81da31ae18fa3c8cb981e442fd65da1e8dc731b0
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2025-07-12 16:39:22 -0300

    Ajustando o links

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,7 +1,19 @@
+import styles from './navbar.module.css'
+
 export default function Navbar() {
     return (
         <>
-        <h1>navbar</h1>
+        <nav className={styles.navbarContainer}>
+        <div className={styles.navbarItems}>
+        <img className={styles.logo} src="/imgs/logo.png" alt="" />
+         <div className={styles.navbarLinksContainer}>
+        <a href="/" className={styles.navbarLink}>Home</a>
+        <a href="/" className={styles.navbarLink}>Plates</a>
+        
+        </div>        
+        </div>
+
+        </nav>
      </>   
     )
 }
