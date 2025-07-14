1bb9d8ba5444fdd07cc0876ac802a3fe17dc3f7b
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2025-07-13 22:39:35 -0300

    Colocando os icones

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,4 +1,5 @@
-import styles from './navbar.module.css'
+import styles from './navbar.module.css';
+import { LuShoppingCart, LuUserCircle, LuMenu } from "react-icons/lu"
 
 export default function Navbar() {
     return (
@@ -7,12 +8,14 @@ export default function Navbar() {
         <div className={styles.navbarItems}>
         <img className={styles.logo} src="/imgs/logo.png" alt="" />
          <div className={styles.navbarLinksContainer}>
-        <a href="/" className={styles.navbarLink}>Home</a>
-        <a href="/" className={styles.navbarLink}>Plates</a>
+        <a href="" className={styles.navbarLink}>Home</a>
+        <a href="" className={styles.navbarLink}>Plates</a>
+        <LuShoppingCart lassName={styles.navbarLink}/>
+        <LuUserCircle className={styles.navbarLink}/>
         
         </div>        
         </div>
-
+      
         </nav>
      </>   
     )
