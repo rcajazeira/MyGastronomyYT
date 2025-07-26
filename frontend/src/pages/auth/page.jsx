e0fff07b79a11d57168649fd931fccb9f7038302
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2025-07-26 17:20:25 -0300

    Ajuste do caminho de login

--- frontend/src/pages/auth/page.jsx
+++ frontend/src/pages/auth/page.jsx
@@ -1,5 +1,35 @@
-export default function auth() {
+import { useState } from 'react'
+
+export default function Auth() {
+  const [formType, setFormType] = useState('login')
+
+  const handleChangeFormType = () => {
+    if (formType === 'login') {
+      setFormType('signup')
+    } else {
+      setFormType('login')
+    }
+  }
+
+  if (formType === 'login') {
     return (
-        <h1>auth</h1>
+      <>
+        <h1>Login</h1>
+        <button onClick={handleChangeFormType}>
+          Don't you have an account? Click here
+        </button>
+      </>
     )
+  }
+
+  if (formType === 'signup') {
+    return (
+      <>
+        <h1>Signup</h1>
+        <button onClick={handleChangeFormType}>
+          Already have an account? Click here
+        </button>
+      </>
+    )
+  }
 }
